dd11ac4b980cd4727600c75377d8da15de018d92
Dandan Wu <wudandan@sunseaaiot.com>
2021-03-02 16:23:36 +0800

    feat:storage

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,9 +1,14 @@
+import 'dart:ui';
+
+import 'package:filemanager/app/modules/home/components/activity_sheet/activity_sheet.dart';
 import 'package:filemanager/app/modules/home/components/file_list/file_list.dart';
 import 'package:filemanager/app/modules/home/views/search_bar.dart';
+import 'package:filemanager/app/components/float_button/float_button.dart';
+import 'package:filemanager/app/modules/home/views/storage.dart';
 import 'package:filemanager/constant.dart';
 import 'package:filemanager/util.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:math' as math;
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
@@ -16,18 +21,21 @@ class HomeView extends GetView<HomeController> {
       //   title: Text('HomeView'),
       //   centerTitle: true,
       // ),
+      floatingActionButton: FloatButton(),
+      floatingActionButtonLocation: CustomFloatingActionButtonLocation(
+          FloatingActionButtonLocation.endFloat, 0, -90),
       body: Container(
         padding: EdgeInsets.only(top: Get.context.mediaQueryPadding.top),
         child: Stack(children: [
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
+          CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Container(
                   height: 60,
                   width: double.infinity,
                   padding: EdgeInsets.only(
-                      left: getProportionateScreenWidth(36),
-                      right: getProportionateScreenWidth(36)),
+                      left: getProportionateScreenWidth(kDefaultPadding),
+                      right: getProportionateScreenWidth(kDefaultPadding)),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -35,95 +43,64 @@ class HomeView extends GetView<HomeController> {
                           style: TextStyle(
                             fontFamily: 'SFProDisplay-Semibold',
                             fontSize: kHeroFontSize,
+                            fontWeight: FontWeight.w700,
                             color: const Color(0xff383838),
                           )),
                       IconButton(
                           onPressed: () {},
-                          icon: ClipOval(
-                            child: Image.asset(
-                              'assets/images/header_image.jpeg',
-                              width: getProportionateScreenWidth(40),
-                              height: getProportionateScreenWidth(40),
+                          icon: Container(
+                            width: getProportionateScreenWidth(45),
+                            height: getProportionateScreenWidth(45),
+                            decoration: BoxDecoration(boxShadow: [
+                              BoxShadow(
+                                color: const Color(0xffadc8f6),
+                                offset: Offset(0, 4),
+                                blurRadius: 20,
+                              )
+                            ]),
+                            child: ClipOval(
+                              child: Image.asset(
+                                'assets/images/header_image.jpeg',
+                                width: getProportionateScreenWidth(40),
+                                height: getProportionateScreenWidth(40),
+                              ),
                             ),
                           ))
                     ],
                   ),
                 ),
-                SizedBox(
+              ),
+              SliverToBoxAdapter(
+                child: SizedBox(
                   height: kDefaultPadding / 2,
                 ),
-                SearchBar(),
-                SizedBox(
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  child: SearchBar(),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: SizedBox(
                   height: kDefaultPadding / 2,
                 ),
-                Container(
+              ),
+              SliverToBoxAdapter(
+                child: Container(
                   height: 184,
                   width: double.infinity,
                   decoration: BoxDecoration(color: Colors.red),
-                  child: Text('storage'),
+                  padding: EdgeInsets.symmetric(
+                      horizontal: kDefaultPadding,
+                      vertical: kDefaultPadding / 2),
+                  child: Storage(),
                 ),
-                Container(
-                  width: double.infinity,
-                  height: 1000,
-                  decoration: BoxDecoration(color: Colors.red[50]),
-                  child: FileList(),
-                ),
-              ],
-            ),
+              ),
+              FileList()
+            ],
           ),
-          SizedBox.expand(
-            child: DraggableScrollableSheet(
-              initialChildSize: 0.2,
-              maxChildSize: 0.8,
-              minChildSize: 0.1,
-              builder: (BuildContext context, myscrollController) {
-                return Container(
-                  decoration: BoxDecoration(color: Colors.red),
-                  margin: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: SingleChildScrollView(
-                    controller: myscrollController,
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 150.0,
-                          color: Colors.deepOrangeAccent,
-                          child: Center(
-                            child: Text(
-                              'Container 1',
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          height: 150.0,
-                          color: Colors.orangeAccent,
-                          child: Center(
-                            child: Text(
-                              'Container 2',
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          height: 150.0,
-                          color: Colors.indigoAccent,
-                          child: Center(
-                            child: Text(
-                              'Container 3',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+          ActivitySheet(),
         ]),
       ),
     );
