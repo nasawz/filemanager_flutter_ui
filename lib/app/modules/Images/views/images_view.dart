cbed764300d1d42751ad28dc93056d949cbbb30a
nasa.wang <nasa.wang@nicesn.com>
2021-03-02 12:08:58 +0800

    feat: images WIP

--- lib/app/modules/Images/views/images_view.dart
+++ lib/app/modules/Images/views/images_view.dart
@@ -1,5 +1,8 @@
+import 'package:filemanager/app/modules/Images/components/top_tool_bar/top_tool_bar.dart';
+import 'package:filemanager/constant.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 
 import '../controllers/images_controller.dart';
@@ -7,15 +10,84 @@ import '../controllers/images_controller.dart';
 class ImagesView extends GetView<ImagesController> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.black,
+    ));
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ImagesView'),
-        centerTitle: true,
+      body: Column(
+        children: [
+          SizedBox(
+            height: Get.context.mediaQueryPadding.top,
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(color: kSecondaryColor),
+              child: Column(
+                children: [
+                  TopToolBar(),
+                  Expanded(
+                    child: StaggeredGridView.countBuilder(
+                      padding: EdgeInsets.all(kDefaultPadding),
+                      crossAxisCount: 4,
+                      itemCount: 18,
+                      itemBuilder: (BuildContext context, int index) =>
+                          index == 0
+                              ? Title()
+                              : ImageCard(
+                                  index: index,
+                                ),
+                      staggeredTileBuilder: (int index) =>
+                          new StaggeredTile.count(2, 1),
+                      mainAxisSpacing: 4.0,
+                      crossAxisSpacing: 4.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'ImagesView is working',
-          style: TextStyle(fontSize: 20),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return // Adobe XD layer: 'Images' (text)
+        Text(
+      'Images ',
+      style: TextStyle(
+        fontFamily: 'SFProText-Semibold',
+        fontSize: 20,
+        color: const Color(0xff383f4b),
+      ),
+      textAlign: TextAlign.left,
+    );
+  }
+}
+
+class ImageCard extends StatelessWidget {
+  final index;
+  const ImageCard({
+    Key key,
+    this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.green,
+      child: new Center(
+        child: new CircleAvatar(
+          backgroundColor: Colors.white,
+          child: new Text('$index'),
         ),
       ),
     );
