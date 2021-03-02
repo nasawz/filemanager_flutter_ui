c656b9cb6ea51eac0f68d29eb06ca571e37825b6
hy6508 <707230595@qq.com>
2021-03-02 15:35:41 +0800

    feat: item布局

--- lib/app/modules/activity/views/activity_item.dart
+++ lib/app/modules/activity/views/activity_item.dart
@@ -6,10 +6,32 @@ class ActivityItem extends StatelessWidget {
     return Column(
       children: [
         Container(
-          child: Text('Item'),
-        ),
-        Container(
-          child: Text('Item'),
+          width: 314.0,
+          height: 170.0,
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    child: Text('原点'),
+                  ),
+                  Container(
+                    child: Text('虚线'),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    child: Text('标题'),
+                  ),
+                  Container(
+                    child: Text('图片'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ],
     );
