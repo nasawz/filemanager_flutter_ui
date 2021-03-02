df681255601592ce374487a315eb9dd02beca296
hy6508 <707230595@qq.com>
2021-03-02 17:36:36 +0800

    fix: 调整页面层级

--- lib/app/modules/activity/views/activity_view.dart
+++ lib/app/modules/activity/views/activity_view.dart
@@ -8,12 +8,63 @@ import '../controllers/activity_controller.dart';
 import './activity_item.dart';
 
 final data = [
-  {'status': 1, 'time': 'Few seconds ago'},
-  {'status': 1, 'time': '20 minutes ago'},
-  {'status': 0, 'time': '2 hours ago'},
-  {'status': 0, 'time': '4 hours ago'},
-  {'status': 0, 'time': '6 hours ago'},
-  {'status': 0, 'time': '8 hours ago'}
+  {
+    'status': 1,
+    'time': 'Few seconds ago',
+  },
+  {
+    'status': 1,
+    'time': '20 minutes ago',
+    'bitmaps': [
+      'assets/images/Bitmap1.png',
+      'assets/images/Bitmap2.png',
+      'assets/images/Bitmap3.png',
+      'assets/images/Bitmap4.png'
+    ]
+  },
+  {
+    'status': 0,
+    'time': '2 hours ago',
+    'bitmaps': [
+      'assets/images/Bitmap1.png',
+      'assets/images/Bitmap2.png',
+      'assets/images/Bitmap3.png',
+      'assets/images/Bitmap4.png',
+      'assets/images/Bitmap1.png',
+      'assets/images/Bitmap2.png',
+      'assets/images/Bitmap3.png',
+    ]
+  },
+  {
+    'status': 0,
+    'time': '4 hours ago',
+    'bitmaps': [
+      'assets/images/Bitmap4.png',
+      'assets/images/Bitmap2.png',
+      'assets/images/Bitmap3.png',
+      'assets/images/Bitmap1.png'
+    ]
+  },
+  {
+    'status': 0,
+    'time': '6 hours ago',
+    'bitmaps': [
+      'assets/images/Bitmap2.png',
+      'assets/images/Bitmap5.png',
+      'assets/images/Bitmap6.png',
+      'assets/images/Bitmap4.png'
+    ]
+  },
+  {
+    'status': 0,
+    'time': '8 hours ago',
+    'bitmaps': [
+      'assets/images/Bitmap5.png',
+      'assets/images/Bitmap6.png',
+      'assets/images/Bitmap4.png',
+      'assets/images/Bitmap1.png'
+    ]
+  }
 ];
 
 class ActivityView extends GetView<ActivityController> {
@@ -33,58 +84,58 @@ class ActivityView extends GetView<ActivityController> {
             child: Container(
               width: double.infinity,
               decoration: BoxDecoration(color: kBackgroundColor),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: kBackgroundColor,
-                ),
+              child: SingleChildScrollView(
+                // padding: EdgeInsets.all(30.0),
                 child: Container(
-                  width: double.infinity,
                   decoration: BoxDecoration(
-                    color: kSecondaryColor,
-                    borderRadius: BorderRadius.only(
-                      topLeft: const Radius.circular(50.0),
-                      topRight: const Radius.circular(50.0),
-                    ),
+                    color: kBackgroundColor,
                   ),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        height: 60,
-                        alignment: Alignment.center,
-                        child: Container(
-                          width: 40,
-                          height: 5,
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: kSecondaryColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(50.0),
+                        topRight: const Radius.circular(50.0),
+                      ),
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 60,
+                          alignment: Alignment.center,
+                          child: Container(
+                            width: 40,
+                            height: 5,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Container(
-                        height: 40,
-                        width: 240,
-                        margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Last Activity',
-                          style: TextStyle(
-                            fontFamily: 'SFProDisplay-Semibold',
-                            fontSize: 30,
-                            // color: const Color(0xff383838),
+                        Container(
+                          height: 40,
+                          width: 240,
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 40),
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Last Activity',
+                            style: TextStyle(
+                              fontFamily: 'SFProDisplay-Semibold',
+                              fontSize: 30,
+                              // color: const Color(0xff383838),
+                            ),
+                            textAlign: TextAlign.left,
                           ),
-                          textAlign: TextAlign.left,
                         ),
-                      ),
-                      SingleChildScrollView(
-                        padding: EdgeInsets.all(30.0),
-                        child: Column(
+                        Column(
                           children:
                               data.map((item) => ActivityItem(item)).toList(),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
